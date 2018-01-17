0f48dad7b0d2dc4acb2dcdbb04fe4fb0d458cc67
Ray <Ray@DESKTOP-4ESAEOJ>
2018-01-17 10:49:01 +0800

    Pull

--- Team09LogicU/App_Code/UtilClass/DBEntities.cs
+++ Team09LogicU/App_Code/UtilClass/DBEntities.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using Team09LogicU.Models;
+using Team09LogicU;
 namespace Team09LogicU.App_Code.UtilClass
 {
     public class DBEntities
