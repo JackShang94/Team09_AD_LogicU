9de7e2a8707fd435145e6599af4cea0c220eb31e
e0224927 <e0224927@DESKTOP-1A1N39E>
2018-01-18 09:52:42 +0800

    delete uneccessary comments

--- Team09LogicU/App_Code/DAO/DisbursementItemDAO.cs
+++ Team09LogicU/App_Code/DAO/DisbursementItemDAO.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Team09LogicU.App_Code.UtilClass;
 using Team09LogicU.Models;
 
 namespace Team09LogicU.App_Code.DAO
 {
     public class DisbursementItemDAO
     {
-        SA45_Team09_LogicUEntities context = new SA45_Team09_LogicUEntities();
+        SA45_Team09_LogicUEntities context = new DBEntities().getDBInstance();
 
         //1.add retrieved-items to disbursement list
         public void createNewDisburesementItem(int disbId, string itemId, int expectedQty, int actualQty)
@@ -30,19 +31,7 @@ namespace Team09LogicU.App_Code.DAO
             return disbursementItems;
         }
 
-        //public dynamic getDisbursementItemsbyDisbursment(int disbursmentid)
-        //    {
-        //        var hm = from disbst in context.DisbursementItems
-        //                 where disbst.disbursementId == disbursmentid
-        //                 join item in context.Items
-        //                 on disbst.itemId equals item.itemId
-        //                 join DisbursementList in context.DisbursementLists
-        //                 on disbst.disbursementId equals DisbursementList.disbursementId
-        //                 select new { disbst.disbItemSK, disbst.requestQuantity, disbst.actualQuantity, item.description, item.unitOfMeasure, DisbursementList.status };
-        //        return hm.ToList();
-        //    }
-        //what is happening here, I NEED the above ^ method to work. 
-
+       
 
         //Save the input actual qty
         public void savingActualQty(int disbursementItemID, int actualqty)
--- Team09LogicU/Pages/DH_DelegateHeadRole.aspx.cs
+++ Team09LogicU/Pages/DH_DelegateHeadRole.aspx.cs
@@ -4,14 +4,78 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Team09LogicU.Models;
+using Team09LogicU.App_Code.DAO;
 
 namespace Team09LogicU.pages
 {
     public partial class DH_DelegateHeadRole : System.Web.UI.Page
     {
+        DeptStaffDAO deptStaffDAO = new DeptStaffDAO();
+        DelegateDAO delegateDAO = new DelegateDAO();
+        DepartmentDAO deptDAO = new DepartmentDAO();
+        string logInStaffId;
+        string logInRole;
+        string currentHeadId;
+        string selectedEmpName;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                logInStaffId = "emp001";//this is assumption
+                logInRole = deptStaffDAO.findStaffByID(logInStaffId).role;
+                if (logInRole == "head")
+                {
+                    List<DeptStaff> staffList = deptStaffDAO.findOnlyEmployee("ZOOL");//assume it is zool department
+                    employee_dropList.DataSource = staffList;
+                    employee_dropList.DataTextField = "staffName";
+                    employee_dropList.DataValueField = "staffID";
+                    employee_dropList.DataBind();
+                    delegateStf_label.Text = employee_dropList.Text;
 
+                    startDate_textBox.Text = DateTime.Today.ToString();
+                    endDate_textBox.Text = DateTime.Today.AddDays(2).ToString();
+                }
+                else
+                {
+                    employee_dropList.Visible = false;
+                    startDate_textBox.Visible = false;
+                    endDate_textBox.Visible = false;
+                    delegateStf_label.Text = "You are not allowed to delegate now.";
+                    submit_button.Enabled = false;
+
+                }
+                //delegate history
+                string deptId = deptStaffDAO.findStaffByID(logInStaffId).deptID;
+                List<Models.Delegate> dList = delegateDAO.findDelegatesByDepartment(deptId);
+                delegateHistory_ListBox.DataSource = dList;
+                
+                delegateHistory_ListBox.DataBind();
+
+
+            }
+            
+        }
+        protected void submit_button_Click(object sender, EventArgs e)
+        {
+            
+            string staffId = employee_dropList.SelectedValue;
+            DateTime sDate = Convert.ToDateTime(startDate_textBox.Text);
+            DateTime eDate = Convert.ToDateTime(endDate_textBox.Text);
+            delegateDAO.delegateToStaff(staffId, sDate, eDate);
+
+            string currentHeadID =deptStaffDAO.findStaffByID(staffId).Department.headStaffID;
+            delegateDAO.disableHead(currentHeadID);
+            delegateStatus_Label.Text = "Delegated successfully!";
+        }
+
+        protected void terminate_button_Click(object sender, EventArgs e)
+        {
+            if (logInRole == "outOfOfficeHead")
+            {
+
+            }
         }
     }
 }
