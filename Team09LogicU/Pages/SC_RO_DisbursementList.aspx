ef63539948c017bd723a0c4fe4cf6ce41de24504
Alexazhouyang <30647664+Alexazhouyang@users.noreply.github.com>
2018-01-21 22:56:29 +0800

    SC_RO&O UI Update

--- Team09LogicU/Pages/SC_RO_DisbursementList.aspx
+++ Team09LogicU/Pages/SC_RO_DisbursementList.aspx
@@ -3,4 +3,33 @@
     Disbursement List
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <form id="form1" runat="server">
+    <div class="row">
+         <div class="col-lg-12">
+        <div class="card">
+                <div class=" container">
+                    <div class="col-lg-12">
+                   <div class="col-lg-3" style="margin-top:20px">
+                    <asp:Label ID="Label1" runat="server" Text="Select a Department:" CssClass="category"></asp:Label>
+                       <asp:DropDownList ID="DropDownList1" CssClass="form-control" runat="server"></asp:DropDownList>
+                   </div>
+                    <div class="col-lg-10">
+                        <asp:LinkButton ID="LinkButton1" runat="server">View Disbursement List History</asp:LinkButton>
+                        <asp:GridView ID="GridView1" runat="server" CssClass="table table-striped table-hover "></asp:GridView>
+                        <div class="col-lg-10">
+                        <asp:Label ID="Label2" runat="server" Text="Label">Collection Point:</asp:Label>
+                          </div>  </div></div>
+                        <div class="col-lg-3" style=" margin-top:40px;margin-bottom:20px">
+                        <asp:Button ID="Button1" runat="server" CssClass="btn btn-primary btn-wd btn-fill" Text="Inform representative" />
+                       </div>
+                     <div class="col-lg-3 " style="margin-top:40px;margin-bottom:20px">
+                        <asp:Button ID="Button2" runat="server" CssClass="btn btn-primary btn-wd " Text="Generate QR Code" />
+                       </div>
+                       </div>
+                    
+                </div>
+            
+        </div>
+    </div>
+    </form>
 </asp:Content>
--- Team09LogicU/Pages/SC_RO_DisbursementListHistory.aspx.designer.cs
+++ Team09LogicU/Pages/SC_RO_DisbursementListHistory.aspx.designer.cs
@@ -7,10 +7,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Team09LogicU.pages {
+namespace Team09LogicU.Pages {
     
     
-    public partial class SC_RO_DisbursementList {
+    public partial class SC_RO_DisbursementListHistory {
         
         /// <summary>
         /// form1 control.
@@ -29,6 +29,24 @@ namespace Team09LogicU.pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
         
         /// <summary>
         /// DropDownList1 control.
@@ -74,14 +92,5 @@ namespace Team09LogicU.pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// Button2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
