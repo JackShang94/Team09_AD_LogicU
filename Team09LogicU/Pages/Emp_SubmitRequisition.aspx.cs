19023b7aa5ba48a91ac5a2bbb8cf6324c9bced57
chenziqing <chenziqing0726@gmail.com>
2018-01-23 00:48:01 +0800

    fixed part of the <mai hua he> stupid "req" typo bugs

--- Team09LogicU/Pages/Emp_SubmitRequisition.aspx.cs
+++ Team09LogicU/Pages/Emp_SubmitRequisition.aspx.cs
@@ -46,7 +46,7 @@ namespace Team09LogicU.pages
                 string role = Session["loginRole"].ToString();
                 //role = "emp";
                 //Session["loginRole"] = role;
-                if (role !="req"&& role!="emp")
+                if (role !="rep"&& role!="emp")
                 {
                     HttpContext.Current.Response.Redirect("login.aspx");
                     return;
