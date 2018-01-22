6e4e9f568520d7ddd7600a51c1a765600e41ed3d
chenziqing <chenziqing0726@gmail.com>
2018-01-22 21:14:26 +0800

    commit

---
+++ Team09LogicU/Pages/SC_Inv_ManageReorder.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Team09LogicU.Models;
+using Team09LogicU.App_Code.DAO;
+using Team09LogicU.App_Code.UtilClass;
+
+namespace Team09LogicU.Pages
+{
+    public partial class SC_Inv_ManageReorder : System.Web.UI.Page
+    {
+        StoreStaffDAO storeStaffDAO = new StoreStaffDAO();
+        ItemDAO itemDAO = new ItemDAO();
+        SupplierDAO supDAO = new SupplierDAO();
+        SupplierItemDAO supItemDAO = new SupplierItemDAO();       
+        PurchaseOrderDAO PoDAO = new PurchaseOrderDAO();
+        PurchaseOrderItemDAO POItemDAO = new PurchaseOrderItemDAO();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool isSelected = CheckBox1.Checked;
+            if (isSelected)
+            {
+                //this.DisplayLowStockItem();
+            }
+            this.BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            List<ReorderItem> reorderList = new List<ReorderItem>();
+
+            reorderList = itemDAO.findItemList();
+            GridView_reorderList.DataSource = reorderList;
+            GridView_reorderList.DataBind();
+
+        }
+
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView_reorderList.EditIndex = e.NewEditIndex;
+            BindGrid();
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void OnRowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView_reorderList.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if ((e.Row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    string itemID = (e.Row.FindControl("lblItemID") as Label).Text;
+                    DropDownList ddlSupplier = e.Row.FindControl("ddlSuppliers") as DropDownList;
+                    ddlSupplier.DataSource = supItemDAO.findSupplierListByItemID(itemID);
+                    ddlSupplier.AppendDataBoundItems = true;
+                    ddlSupplier.DataTextField = "supplierID";
+                    ddlSupplier.DataValueField = "supplierID";
+                    ddlSupplier.DataBind();
+                   
+                }
+
+            }
+        }
+
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Team09LogicU/Pages/SC_Inv_ManageReorder.aspx.designer.cs
+++ Team09LogicU/Pages/SC_Inv_ManageReorder.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace Team09LogicU.Pages {
     
     
-    public partial class SC_Inv_Reorder {
+    public partial class SC_Inv_ManageReorder {
         
         /// <summary>
         /// form1 control.
@@ -22,66 +22,57 @@ namespace Team09LogicU.Pages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Label3 control.
+        /// CheckBox1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
         
         /// <summary>
-        /// dropdownlist_Catagory control.
+        /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList dropdownlist_Catagory;
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
-        /// Button_Search control.
+        /// UpdatePanel1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button_Search;
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
         
         /// <summary>
-        /// LinkButton_ViewAllPurchaseorders control.
+        /// GridView_reorderList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton_ViewAllPurchaseorders;
+        protected global::System.Web.UI.WebControls.GridView GridView_reorderList;
         
         /// <summary>
-        /// GridView1 control.
+        /// BtnSubmit control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.Button BtnSubmit;
         
         /// <summary>
-        /// Btn_Submit control.
+        /// BtnCancel control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Btn_Submit;
-        
-        /// <summary>
-        /// Btn_Cancel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Btn_Cancel;
+        protected global::System.Web.UI.WebControls.Button BtnCancel;
     }
 }
