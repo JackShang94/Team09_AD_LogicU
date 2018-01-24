c74e25a791dd210c3d096734b115a116310b5b9b
JackShang94 <32633900+JackShang94@users.noreply.github.com>
2018-01-24 11:52:31 +0800

    fix login bug and finish email class

--- Team09LogicU/App_Code/UtilClass/Email.cs
+++ Team09LogicU/App_Code/UtilClass/Email.cs
@@ -26,6 +26,42 @@ namespace Team09LogicU.App_Code.UtilClass
             client.Send(mm);         
         }        
 
+        public void sendFeedBackEmailToEmployee(string employeeName, string headName, string requisitionDate, string requisitionStatus)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new System.Net.NetworkCredential
+            (@"sateam09@gmail.com", "password!123");
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            MailMessage mm = new MailMessage("sateam09@gmail.com", "shangzhengxiang@163.com");           
+            mm.Subject = "Requisition  Status Update Attention !";
+            mm.IsBodyHtml = true;
+            mm.Body = "Dear " + employeeName + ",<br/><br/>" + "&nbsp;&nbsp;&nbsp; Please note that your requisition in "+ requisitionDate + " has been <font style='font - family:verdana; color: red'> " + requisitionStatus + "</font> by  head " + headName + " in your department. "
+                   + "<br/>" + "&nbsp;&nbsp;&nbsp; You may access your requisitions by logging onto the Stationary Store system."
+                   + " <br/><br/>Thank you.<br/>The Logic U Stationary Department team.";
+
+            client.Send(mm);
+        }
+
+        public void sendRepNotificationToEmployee(string staffName,string headName)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new System.Net.NetworkCredential
+            (@"sateam09@gmail.com", "password!123");
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            MailMessage mm = new MailMessage("sateam09@gmail.com", "shangzhengxiang@163.com");
+            mm.Subject = "Representative  Attention !";
+            mm.IsBodyHtml = true;
+            mm.Body = "Dear " + staffName + ",<br/><br/>" + "&nbsp;&nbsp;&nbsp; Please note that your head " + headName+ " in your department has assigned you as department representative. "
+                   + "<br/>" + "&nbsp;&nbsp;&nbsp; You may access your requisitions by logging onto the Stationary Store system."
+                   + " <br/><br/>Thank you.<br/>The Logic U Stationary Department team.";
+
+            client.Send(mm);
+        }
+
         public void sendDisbursementEmailToRep(string repName, string disbursementDate, string collectionPoint)
         {
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
@@ -44,7 +80,40 @@ namespace Team09LogicU.App_Code.UtilClass
             client.Send(mm);
         }
 
+        public void sendAdjustmentEmailToSupervisor(string clerkName, string supervisorName)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new System.Net.NetworkCredential
+            (@"sateam09@gmail.com", "password!123");
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
 
+            MailMessage mm = new MailMessage("sateam09@gmail.com", "shangzhengxiang@163.com");
+            mm.Subject = "New Adjustment Voucher  Attention !";
+            mm.IsBodyHtml = true;
+            mm.Body = "Dear " + supervisorName + ",<br/><br/>" + "&nbsp;&nbsp;&nbsp; Please note that there has been a new Adjustment Voucher added by the clerk " + clerkName + " in your store for your attention. "
+                   + "<br/>" + "&nbsp;&nbsp;&nbsp; You may access your requisitions by logging onto the Stationary Store system."
+                   + " <br/><br/>Thank you.<br/>The Logic U Stationary Department team.";
 
+            client.Send(mm);
+        }
+
+        public void sendAdjustmentEmailToManager(string supervisorName, string managerName)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new System.Net.NetworkCredential
+            (@"sateam09@gmail.com", "password!123");
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            MailMessage mm = new MailMessage("sateam09@gmail.com", "shangzhengxiang@163.com");
+            mm.Subject = "New Adjustment Voucher  Attention !";
+            mm.IsBodyHtml = true;
+            mm.Body = "Dear " + managerName + ",<br/><br/>" + "&nbsp;&nbsp;&nbsp; Please note that there has been a new Adjustment Voucher added by the supervisor " + supervisorName + " in your store for your attention. "
+                   + "<br/>" + "&nbsp;&nbsp;&nbsp; You may access your requisitions by logging onto the Stationary Store system."
+                   + " <br/><br/>Thank you.<br/>The Logic U Stationary Department team.";
+
+            client.Send(mm);
+        }
     }
 }
