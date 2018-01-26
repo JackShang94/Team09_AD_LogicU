574dc417086b4dc995f8bfa87bbe3fe7c361d0df
e0224927 <e0224927@DESKTOP-1A1N39E>
2018-01-26 13:31:00 +0800

    Modified EditSupplier

--- Team09LogicU/App_Code/DAO/SupplierDAO.cs
+++ Team09LogicU/App_Code/DAO/SupplierDAO.cs
@@ -62,12 +62,10 @@ namespace Team09LogicU.App_Code.DAO
             context.SaveChanges();
         }
 
-        public  void updateSupplier(string supplierCode, string supplierName, string gstRegistrationNo, string address, string fax, string phone, string contactName)
+        public  void updateSupplier(string supplierCode,string gstRegistrationNo, string address, string fax, string phone, string contactName)
         {
             Supplier sl = context.Suppliers.Where(x => x.supplierID == supplierCode).First();
 
-            sl.supplierID = supplierCode;
-            sl.supplierName = supplierName;
             sl.gstRegistrationNo = gstRegistrationNo;
             sl.address = address;
             sl.fax = fax;
