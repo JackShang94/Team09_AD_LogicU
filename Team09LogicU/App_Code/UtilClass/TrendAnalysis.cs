239a4b3de788598750518b8d58c6570996067f63
JackShang94 <32633900+JackShang94@users.noreply.github.com>
2018-01-26 09:01:21 +0800

    Add trend analysis

---
+++ Team09LogicU/App_Code/UtilClass/TrendAnalysis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team09LogicU.App_Code.UtilClass
+{
+    public class TrendAnalysis
+    {
+
+    }
+}
