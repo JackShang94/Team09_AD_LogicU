491237666ee21798972e76534ab85c05b36def22
xuxu <e0224927@DESKTOP-1A1N39E>
2018-01-27 17:46:03 +0800

    finish ItemService

--- Team09LogicU/AndroidServices/IItemService.cs
+++ Team09LogicU/AndroidServices/IItemService.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using Team09LogicU.App_Code.DAO;
+using Team09LogicU.Models;
+using System.ServiceModel.Web;
 
 namespace Team09LogicU.AndroidServices
 {
@@ -12,6 +15,46 @@ namespace Team09LogicU.AndroidServices
     public interface IItemService
     {
         [OperationContract]
-        void DoWork();
+        [WebGet(UriTemplate = "/Item/{cat}", ResponseFormat = WebMessageFormat.Json)]
+        List<WCFItem> findItemByCat(string cat);
+    }
+
+    [DataContract]
+    public class WCFItem
+    {
+        string itemID;
+        string categoryID;
+        string description;
+
+
+        public static WCFItem Make(string itemID, string categoryID, string description)
+        {
+            WCFItem i = new WCFItem();
+            i.itemID = itemID;
+            i.categoryID = categoryID;
+            i.description = description;
+            return i;
+        }
+
+        [DataMember]
+        public string ItemID
+        {
+            get { return itemID; }
+            set { itemID = value; }
+        }
+
+        [DataMember]
+        public string CategoryID
+        {
+            get { return categoryID; }
+            set { categoryID = value; }
+        }
+
+        [DataMember]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
     }
 }
