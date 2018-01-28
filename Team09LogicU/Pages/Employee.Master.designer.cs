e16e6e17da6570f133dac4eaf6293620cc1976c9
Alexazhouyang <30647664+Alexazhouyang@users.noreply.github.com>
2018-01-28 17:13:25 +0800

    Notification masterpage Update；label_number ID= notificationNum

--- Team09LogicU/Pages/Employee.Master.designer.cs
+++ Team09LogicU/Pages/Employee.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Team09LogicU.pages {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
+        /// notificationNum control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label notificationNum;
+        
+        /// <summary>
         /// ContentPlaceHolder2 control.
         /// </summary>
         /// <remarks>
