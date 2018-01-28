50444f5c3b7eef4e25616d5b078105f73ab5c1f6
Alexazhouyang <30647664+Alexazhouyang@users.noreply.github.com>
2018-01-28 16:54:42 +0800

    login/out_show username and logout completed

--- Team09LogicU/Pages/StoreSupervisor.Master.designer.cs
+++ Team09LogicU/Pages/StoreSupervisor.Master.designer.cs
@@ -22,6 +22,24 @@ namespace Team09LogicU.Pages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
